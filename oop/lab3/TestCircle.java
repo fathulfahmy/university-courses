63b7341ad9f4156a8742c5faf7224e1a5ca9459e
patuicode <fathulfmy@gmail.com>
2023-02-16 18:55:34 +0800

    add Cuboid

--- oop/lab3/TestCircle.java
+++ oop/lab3/TestCircle.java
@@ -2,11 +2,5 @@ public class TestCircle {
 
     public static void main(String[] args) {
         Circle circle1, circle2;
-        circle1.setRadius(2.3);
-        circle2.setRadius(4.8);
-        circle1.area();
-        circle1.circumference();
-        circle2.area();
-        circle2.circumference();
     }
 }

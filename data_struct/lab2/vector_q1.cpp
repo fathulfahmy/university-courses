4fea3b3487a7d5c164185a0a85ccb6c8b0817f5c
fathulfahmy <fathulfmy@gmail.com>
2023-04-11 21:55:36 +0800

    Updated vector q1_cpp

--- data_struct/lab2/vector_q1.cpp
+++ data_struct/lab2/vector_q1.cpp
@@ -8,13 +8,5 @@ int main()
     // Q: assign iterator i to vector that hold float in reverse order
     // A: code below
     vector<float>::reverse_iterator i;
-
-    // breakdown
-    // 1. assign iterator i
-    reverse_iterator i;
-
-    // 2. vector hold float
-    vector<float>;
-
     return 0;
 }
